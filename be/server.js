0bebe9e802e5871dbc73015e0eab16a62b85fce7
Alexangelo Orozco Gutierrez <alexangeloorozco@icloud.com>
2026-04-16 11:47:43 -0700

    base routes with TODOs

--- be/server.js
+++ be/server.js
@@ -9,6 +9,36 @@ app.get("/", (_req, res) => {
   res.json({ message: "VecDb backend is running" });
 });
 
+app.post("/add-image", (_req, res) => {
+  // TODO: post method, read user image, create embedding, and add to database
+  res.json({ message: "Added Image to database" });
+});
+
+app.get("/get-all-images", (_req, res) => {
+  // TODO: get method, returns all images
+  res.json({ message: "Retrieved All Images" });
+});
+
+app.get("/get-image-by-image", (_req, res) => {
+  // TODO: get method, read user image, create embedding, and search database for similar images
+  res.json({ message: "Retrieved Image by Image" });
+});
+
+app.get("/get-image-by-text", (_req, res) => {
+  // TODO: get method, read user text, create embedding, and search database for similar images
+  res.json({ message: "Retrieved Image by Text" });
+});
+
+app.patch("/update-image", (_req, res) => {
+  // TODO: patch method, check what data is going to be updated, if image is updated recreate embedding
+  res.json({ message: "Updated Image in database" });
+});
+
+app.delete("/delete-image", (_req, res) => {
+  // TODO: delete method, deletes image from database
+  res.json({ message: "Deleted Image from database" });
+});
+
 app.get("/health", (_req, res) => {
   res.status(200).json({ status: "ok" });
 });
